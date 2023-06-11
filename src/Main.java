d78388ba652d85adcb88cc4d52df1ea8cee703fc
unknown <you@example.com>
2023-06-11 15:01:57 +0200

    Dodanie main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import controllers.RunController;
+
+/**
+  Run program
+ */
+public class Main {
+    /**
+     Initialize controller
+     */
+    public static void main(String[] args) {
+        (new RunController()).run();
+    }
+}
